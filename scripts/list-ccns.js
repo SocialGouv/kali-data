d6e0a0a0889e1edbb41a3b795ef6ba2909026c41
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-08-19 17:23:08 +0200

    feat: add yarn run list

--- scripts/list-ccns.js
+++ scripts/list-ccns.js
@@ -1,10 +1,27 @@
 const conventions = require("../data/index.json");
 
+// produce a markdown table from existing data/index.json
+
 console.log(`id | idcc | title | date_publi`);
-console.log(`----|----|----|----`);
+console.log(`---|------|-------|-----------`);
+
+const clean = str =>
+  ("" + (str || ""))
+    .replace(/&#13;/g, "")
+    .replace(/\n/g, " ")
+    .trim();
+
+const cleanDate = dte =>
+  dte
+    .split("T")[0]
+    .split("-")
+    .reverse()
+    .join("/");
 
 conventions.forEach(convention => {
   console.log(
-    `${convention.id} | ${convention.num} | ${convention.titre} | ${convention.date_publi}`
+    `${clean(convention.id)} | ${clean(convention.num)} | ${clean(
+      convention.titre
+    )} | ${cleanDate(convention.date_publi)}`
   );
 });
