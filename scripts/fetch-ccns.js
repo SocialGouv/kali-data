ed67da9a16eb153128cb0f1eb874095f9e776bf0
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-08-22 10:58:27 +0200

    feat: make data a syntax-tree

--- scripts/fetch-ccns.js
+++ scripts/fetch-ccns.js
@@ -6,15 +6,39 @@ import { filterData } from "./fetch-steps/filter-data";
 import { embedCCNTexts } from "./fetch-steps/embed-ccn-texts";
 
 // fetch all conventions from index.json
-import conventions from "../data/";
+import conventions from "../data/index.json";
+
+// convert to syntax-tree format : flatten articles|sections in children
+const astify = (node, depth = 0) => ({
+  type: "section",
+  intOrdre: node.intOrdre,
+  title: node.title,
+  id: node.id,
+  etat: node.etat,
+  ...(depth === 0 && {
+    num: node.num,
+    shortTitle: node.shortTitle
+  }),
+  children: [
+    ...((node.sections && node.sections.map(node => astify(node, depth + 1))) ||
+      []),
+    ...((node.articles &&
+      node.articles.map(article => ({
+        type: "article",
+        ...article
+      }))) ||
+      [])
+  ]
+});
 
 const fetchCCN = id =>
   getCCNContainer(id)
     .then(filterData) // first pass to reduce the number of text calls
     .then(embedCCNTexts)
-    .then(filterData); // second and final pass to trim and order additional texts
+    .then(filterData) // second and final pass to trim and order additional texts
+    .then(astify);
 
-// for each convention, fetch convention conteneur, populate conteneur texts, and ouput to JSON file.
+//for each convention, fetch convention conteneur, populate conteneur texts, and ouput to JSON file.
 pMap(
   conventions,
   convention => {
@@ -33,11 +57,10 @@ pMap(
   .catch(console.log);
 
 // IDCC 1747 - Convention collective nationale des activités industrielles de boulangerie et pâtisserie du 13 juillet 1993.
-/*
-fetchCCN("KALICONT000005635173")
-  .then(data => {
-    fs.writeFileSync("./data/test.json", JSON.stringify(data, null, 2));
-    console.log("wrote ./data/test.json");
-  })
-  .catch(console.error);
-*/
+
+// fetchCCN("KALICONT000005635173")
+//   .then(data => {
+//     fs.writeFileSync("./data/test.json", JSON.stringify(data, null, 2));
+//     console.log("wrote ./data/test.json");
+//   })
+//   .catch(console.error);
