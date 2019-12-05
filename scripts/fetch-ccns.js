772e3c8809870283f73ef601fcd019391918a499
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-12-05 16:57:01 +0100

    fix(script): add CCN validity check

--- scripts/fetch-ccns.js
+++ scripts/fetch-ccns.js
@@ -13,43 +13,60 @@ import conventions from "../data/index.json";
 
 const fetchCCN = id =>
   getCCNContainer(id)
+    //.then(re => console.log("getContainer", re) || re)
     .then(filterData) // first pass to reduce the number of text calls
     .then(embedCCNTexts) // add textes attaches & salaires
     .then(filterData) // second and final pass to trim and order additional texts
-    .then(astify); // convert to AST format
+    .then(astify) // convert to AST format
+    .then(checkCcn); // check validity
 
 const strfy = data => JSON.stringify(data, null, 2);
 
+const checkCcn = ccn => {
+  if (ccn.data.num.length != 4) {
+    throw new Error(`ccn ${ccn.data.id}: invalid num "${ccn.data.num}"`);
+  }
+  if (ccn.children.length === 0) {
+    throw new Error(`ccn ${ccn.data.id}: no children"`);
+  }
+  return ccn;
+};
+
 //for each convention, fetch convention conteneur, populate conteneur texts, and ouput to JSON file.
-pMap(
-  conventions,
-  convention => {
-    const filePath = path.join(__dirname, `../data/${convention.id}.json`);
-    console.log(`fetch ${convention.id}`);
-    return fetchCCN(convention.id)
-      .then(data => {
+const fetchAllConventions = () =>
+  pMap(
+    conventions,
+    convention => {
+      const filePath = path.join(__dirname, `../data/${convention.id}.json`);
+      console.log(`fetch ${convention.id}`);
+      return fetchCCN(convention.id).then(data => {
         const exists = fs.existsSync(filePath);
         const changed = strfy(exists && require(filePath)) !== strfy(data); // poor-man compare
         if (changed) {
           fs.writeFileSync(filePath, strfy(data));
           console.log(`wrote ${filePath}`);
         }
-      })
-      .catch(console.error);
-  },
-  { concurrency: 3 }
-)
-  .then(() => console.log("done !"))
-  .catch(e => {
-    console.log(e);
-    throw e;
-  });
+      });
+      //  .catch(console.error);
+    },
+    { concurrency: 1, stopOnError: true }
+  )
+    .then(() => console.log("done !"))
+    .catch(e => {
+      console.error(e);
+      throw e;
+    });
+
+fetchAllConventions();
 
 // IDCC 1747 - Convention collective nationale des activités industrielles de boulangerie et pâtisserie du 13 juillet 1993.
 
-// fetchCCN("KALICONT000005635173")
+// fetchCCN("KALICONT000005635269")
 //   .then(data => {
 //     fs.writeFileSync("./data/test.json", JSON.stringify(data, null, 2));
 //     console.log("wrote ./data/test.json");
 //   })
-//   .catch(console.error);
+//   .catch(e => {
+//     console.error(e);
+//     throw e;
+//   });
