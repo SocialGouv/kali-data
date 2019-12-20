7d1b8e9671344c3bc6ced8ddfb51808b88d84d30
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-12-20 05:54:03 +0100

    feat: dont fetch textes annexes et salaires
    BREAKING CHANGE

--- src/fetchCcn.js
+++ src/fetchCcn.js
@@ -1,5 +1,4 @@
 import { getCcnContainer } from "./getCcnContainer";
-import { embedCcnTexts } from "./embedCcnTexts";
 
 import { filterData } from "../src/filterData";
 import astify from "../src/astify";
@@ -8,8 +7,6 @@ const fetchCcn = id =>
   getCcnContainer(id)
     //.then(re => console.log("getContainer", re) || re)
     .then(filterData) // first pass to reduce the number of text calls
-    .then(embedCcnTexts) // add textes attaches & salaires
-    .then(filterData) // second and final pass to trim and order additional texts
     .then(astify); // convert to AST format
 
 export default fetchCcn;
