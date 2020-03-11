d72347334409876c06f4dd7eada02ed99fa3d9a3
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-03-11 11:52:48 +0100

    feat: re-include additional articles

--- src/fetchCcn.js
+++ src/fetchCcn.js
@@ -1,12 +1,15 @@
+import { embedCcnTexts } from "./embedCcnTexts";
 import { getCcnContainer } from "./getCcnContainer";
 
-import { filterData } from "../src/filterData";
 import astify from "../src/astify";
+import { filterData } from "../src/filterData";
 
 const fetchCcn = id =>
   getCcnContainer(id)
     //.then(re => console.log("getContainer", re) || re)
     .then(filterData) // first pass to reduce the number of text calls
+    .then(embedCcnTexts) // add textes attaches & salaires
+    .then(filterData) // second and final pass to trim and order additional texts
     .then(astify); // convert to AST format
 
 export default fetchCcn;
