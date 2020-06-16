faf87bb771d01cf4f89f8219c158a10f371bb216
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-06-16 08:12:40 +0200

    refactor(generators): clean check script

--- scripts/generators/check.js
+++ scripts/generators/check.js
@@ -1,24 +1,37 @@
+// @ts-check
+
 import log from "npmlog";
-import findAllAfter from "unist-util-find-all-after";
+import unistUtilFind from "unist-util-find";
 
 import { getAgreement, getAgreements } from "../..";
 
 log.enableColor();
 const INDEXED_AGREEMENTS = getAgreements();
 
-const ccnWithUrl = INDEXED_AGREEMENTS.filter(convention => !!convention.url);
+/**
+ * @param {KaliData.IndexedAgreement} convention
+ *
+ * @returns {boolean}
+ */
+const containsUrl = convention => typeof convention.url === "string";
 
-for (const { id } of ccnWithUrl) {
-  const tree = getAgreement(id);
-  const nodes = findAllAfter(
-    tree,
-    1,
-    node => node.type === "section" && node.children && node.children.length === 0,
-  );
+/**
+ * @param {KaliData.AgreementArticle | KaliData.AgreementSection} node
+ *
+ * @returns {boolean}
+ */
+const hasSectionWithNoChild = node =>
+  node.type === "section" && Array.isArray(node.children) && node.children.length === 0;
 
-  if (nodes.length > 0) {
-    log.error("check()", `${id} has ${nodes.length} empty nodes`);
+const agrementsWithUrl = INDEXED_AGREEMENTS.filter(containsUrl);
+agrementsWithUrl.forEach(({ id }) => {
+  const agreement = getAgreement(id);
+  const agreementAdditionalSections = agreement.children.slice(1);
+  const emptyNodes = unistUtilFind(agreementAdditionalSections, hasSectionWithNoChild);
+
+  if (emptyNodes !== undefined) {
+    log.error("check()", `${id} has ${emptyNodes.length} empty nodes.`);
 
     process.exit(-1);
   }
-}
+});
