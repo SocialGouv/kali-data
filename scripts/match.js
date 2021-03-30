6466e965a9db00fe6f2300057964962ef8ba0f05
LionelB <lionel@lumographe.fr>
2021-03-30 09:54:16 +0200

    use flatMap

--- scripts/match.js
+++ scripts/match.js
@@ -17,40 +17,31 @@ log.enableColor();
 log.info("match()", `Indexing articles…`);
 const agreementsIndex = getAgreements();
 
-const flatMap = arr => arr.reduce((a, c) => [...a, ...c], []);
+const articlesIndex = agreementsIndex.flatMap(({ id: agreementId }) => {
+  if (/-\d+$/.test(agreementId)) {
+    return [];
+  }
+  console.warn("getAgreement", agreementId);
 
-const articlesIndex = flatMap(
-  agreementsIndex
-    .map(({ id: agreementId }) => {
-      if (/-\d+$/.test(agreementId)) {
-        return null;
-      }
-      console.warn("getAgreement", agreementId);
+  const agreement = getAgreement(agreementId);
+  const agreementWithFlatArticles =
+    /** @type {{ type: "root", children: KaliData.AgreementArticle }} */
+    (unistUtilFlatFilter(agreement, "article"));
+  if (agreementWithFlatArticles === null || !Array.isArray(agreementWithFlatArticles.children)) {
+    return [];
+  }
 
-      const agreement = getAgreement(agreementId);
-      const agreementWithFlatArticles =
-        /** @type {{ type: "root", children: KaliData.AgreementArticle }} */
-        (unistUtilFlatFilter(agreement, "article"));
-      if (
-        agreementWithFlatArticles === null ||
-        !Array.isArray(agreementWithFlatArticles.children)
-      ) {
-        return null;
-      }
+  const agreementArticles = agreementWithFlatArticles.children.map(
+    ({ data: { cid: articleCid, id: articleId } }) => ({
+      agreementId,
+      articleCid,
+      articleId,
+      path: getArticlePath(agreement, articleCid),
+    }),
+  );
 
-      const agreementArticles = agreementWithFlatArticles.children.map(
-        ({ data: { cid: articleCid, id: articleId } }) => ({
-          agreementId,
-          articleCid,
-          articleId,
-          path: getArticlePath(agreement, articleCid),
-        }),
-      );
-
-      return agreementArticles;
-    }, [])
-    .filter(Boolean),
-);
+  return agreementArticles;
+});
 
 const articlesIndexFilePath = path.join(__dirname, "..", "data", "articles", "index.json");
 log.info("match()", `Writing ${articlesIndexFilePath}…`);
