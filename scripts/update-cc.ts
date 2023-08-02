5690fd08857e5a321c7f1b6c4f68b51effc73997
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-08-02 17:53:44 +0200

    fix: sync cc

--- scripts/update-cc.ts
+++ scripts/update-cc.ts
@@ -67,8 +67,9 @@ function getDifferenceBetweenDataAndXlsx(): {
   workSheetsFromFile[0].data.forEach((row: string[]) => {
     const ccNumber = parseInt(row[0]);
     const ccName = row[1];
-    const ccNameWithoutParenthesis = ccName.replace(/\(.*\)/g, "");
-    if (ccNumber && ccNumber < 5000) {
+    if (ccNumber && ccNumber < 5000 && ccName) {
+      const ccNameWithoutParenthesis = ccName.replace(/\(.*annexée.*\)/gi, "").trim();
+      console.log(ccNameWithoutParenthesis);
       supportedCcXlsx.push({
         name: ccNameWithoutParenthesis,
         num: ccNumber,
@@ -94,17 +95,22 @@ function getDifferenceBetweenDataAndXlsx(): {
   return { ccManquante, ccEnTrop };
 }
 
-async function getKaliInfo(idccNumber: number): Promise<DilaResponse> {
+async function getKaliInfo(idccNumber: number): Promise<DilaResponse | null> {
   if (!process.env.OAUTH_CLIENT_ID || !process.env.OAUTH_CLIENT_SECRET) {
     throw new Error("OAUTH_CLIENT_ID or OAUTH_CLIENT_SECRET is not defined");
   }
-  const result = await dilaApi.fetch({
-    method: "POST",
-    params: {
-      id: idccNumber,
-    },
-    path: "consult/kaliContIdcc",
-  });
+  const result = await dilaApi
+    .fetch({
+      method: "POST",
+      params: {
+        id: idccNumber,
+      },
+      path: "consult/kaliContIdcc",
+    })
+    .catch((err: any) => {
+      console.error(err);
+      return null;
+    });
   return result;
 }
 
@@ -112,33 +118,36 @@ async function main() {
   const { ccManquante, ccEnTrop } = getDifferenceBetweenDataAndXlsx();
   for (const cc of ccManquante) {
     const dilaInfo = await getKaliInfo(cc.num);
-    const kaliInfo: KaliInfo = {
-      type: "convention collective",
-      data: {
-        num: parseInt(dilaInfo.num),
-        id: dilaInfo.id,
-        title: dilaInfo.titre,
-        shortTitle: (dilaInfo.categorisation && dilaInfo.categorisation[0]) ?? dilaInfo.titre,
-        categorisation: dilaInfo.categorisation ?? [],
-      },
-      children: [],
-    };
-    fs.writeFileSync(
-      process.cwd() + `/data/${kaliInfo.data.id}.json`,
-      JSON.stringify(kaliInfo, null, 2),
-      "utf-8",
-    );
-    dataJson.push({
-      active: true,
-      etat: dilaInfo.sections[0].etat,
-      id: kaliInfo.data.id,
-      nature: dilaInfo.nature,
-      num: kaliInfo.data.num,
-      shortTitle: kaliInfo.data.shortTitle,
-      texte_de_base: dilaInfo.texteBaseId[0],
-      title: kaliInfo.data.title,
-      url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=" + kaliInfo.data.id,
-    });
+    if (dilaInfo) {
+      console.log(dilaInfo);
+      const kaliInfo: KaliInfo = {
+        type: "convention collective",
+        data: {
+          num: parseInt(dilaInfo.num),
+          id: dilaInfo.id,
+          title: dilaInfo.titre,
+          shortTitle: (dilaInfo.categorisation && dilaInfo.categorisation[0]) ?? dilaInfo.titre,
+          categorisation: dilaInfo.categorisation ?? [],
+        },
+        children: [],
+      };
+      fs.writeFileSync(
+        process.cwd() + `/data/${kaliInfo.data.id}.json`,
+        JSON.stringify(kaliInfo, null, 2),
+        "utf-8",
+      );
+      dataJson.push({
+        active: true,
+        etat: dilaInfo.sections[0].etat,
+        id: kaliInfo.data.id,
+        nature: dilaInfo.nature,
+        num: kaliInfo.data.num,
+        shortTitle: kaliInfo.data.shortTitle,
+        texte_de_base: dilaInfo.texteBaseId[0],
+        title: kaliInfo.data.title,
+        url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=" + kaliInfo.data.id,
+      });
+    }
   }
   for (const cc of ccEnTrop) {
     const element = dataJson.find(ccIndex => ccIndex.num === cc.num);
