607c121e35977850383022dbc262d3dd640b5421
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-08-21 20:33:24 +0200

    fix: code

--- update-cc/index.ts
+++ update-cc/index.ts
@@ -8,40 +8,39 @@ async function main() {
   const ccs = getInfosCcs();
   const copyDataJson = [...dataJson];
   for (const cc of ccs) {
-    const dilaInfo = await getKaliInfoWithKaliContainerId(cc.id);
-    if (dilaInfo) {
-      const kaliInfo: KaliInfo = {
-        type: "convention collective",
-        data: {
-          num: parseInt(dilaInfo.num),
-          id: dilaInfo.id,
-          title: dilaInfo.titre,
-          shortTitle: (dilaInfo.categorisation && dilaInfo.categorisation[0]) ?? dilaInfo.titre,
-          categorisation: dilaInfo.categorisation ?? [],
-        },
-        children: [],
-      };
-      fs.writeFileSync(
-        process.cwd() + `/data/${kaliInfo.data.id}.json`,
-        JSON.stringify(kaliInfo, null, 2),
-        "utf-8",
-      );
-      copyDataJson.push({
-        active: true,
-        etat: dilaInfo.sections[0].etat,
-        id: kaliInfo.data.id,
-        nature: dilaInfo.nature,
-        num: kaliInfo.data.num,
-        shortTitle: kaliInfo.data.shortTitle,
-        texte_de_base: dilaInfo.texteBaseId[0],
-        title: kaliInfo.data.title,
-        url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=" + kaliInfo.data.id,
-      });
+    const fileName = process.cwd() + `/data/${cc.id}.json`;
+    if (!fs.existsSync(fileName)) {
+      const dilaInfo = await getKaliInfoWithKaliContainerId(cc.id);
+      if (dilaInfo) {
+        const kaliInfo: KaliInfo = {
+          type: "convention collective",
+          data: {
+            num: parseInt(dilaInfo.num),
+            id: dilaInfo.id,
+            title: dilaInfo.titre,
+            shortTitle: (dilaInfo.categorisation && dilaInfo.categorisation[0]) ?? dilaInfo.titre,
+            categorisation: dilaInfo.categorisation ?? [],
+          },
+          children: [],
+        };
+        fs.writeFileSync(fileName, JSON.stringify(kaliInfo, null, 4), "utf-8");
+        copyDataJson.push({
+          active: true,
+          etat: dilaInfo.sections[0].etat,
+          id: kaliInfo.data.id,
+          nature: dilaInfo.nature,
+          num: kaliInfo.data.num,
+          shortTitle: kaliInfo.data.shortTitle,
+          texte_de_base: dilaInfo.texteBaseId[0],
+          title: kaliInfo.data.title,
+          url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=" + kaliInfo.data.id,
+        });
+      }
     }
   }
   fs.writeFileSync(
     process.cwd() + "/data/index.json",
-    JSON.stringify(copyDataJson, null, 2),
+    JSON.stringify(copyDataJson, null, 4),
     "utf-8",
   );
 }
