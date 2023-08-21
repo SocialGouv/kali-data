67b3ff9e74422880d2e7e75aba1479ecffa42b6d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-08-21 20:14:53 +0200

    fix: code

--- update-cc/index.ts
+++ update-cc/index.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs = require("fs");
 import dataJson = require("../data/index.json");
 import { getInfosCcs } from "./infos";
 import { getKaliInfoWithKaliContainerId } from "./api";
