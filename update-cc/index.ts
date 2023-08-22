ef35e0a71a6d35a0e2d2a61242b5efaf84396721
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-08-22 15:17:00 +0200

    fix: code

--- update-cc/index.ts
+++ update-cc/index.ts
@@ -30,14 +30,44 @@ async function main() {
           etat: dilaInfo.sections[0].etat,
           id: kaliInfo.data.id,
           nature: dilaInfo.nature,
-          num: kaliInfo.data.num,
+          num: cc.num,
           shortTitle: kaliInfo.data.shortTitle,
           texte_de_base: dilaInfo.texteBaseId[0],
           title: kaliInfo.data.title,
           url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=" + kaliInfo.data.id,
         });
-      } else {
-        console.log(`Convention collective ${cc.num} déjà présente`);
+      }
+    } else {
+      console.log(
+        `Convention collective ${cc.num} déjà présente avec comme container id ${cc.id}}`,
+      );
+      if (!copyDataJson.find(ccJson => ccJson.num === cc.num)) {
+        const dilaInfo = await getKaliInfoWithKaliContainerId(cc.id, cc.num);
+        if (dilaInfo) {
+          const kaliInfo: KaliInfo = {
+            type: "convention collective",
+            data: {
+              num: parseInt(dilaInfo.num),
+              id: dilaInfo.id,
+              title: dilaInfo.titre,
+              shortTitle: (dilaInfo.categorisation && dilaInfo.categorisation[0]) ?? dilaInfo.titre,
+              categorisation: dilaInfo.categorisation ?? [],
+            },
+            children: [],
+          };
+          fs.writeFileSync(fileName, JSON.stringify(kaliInfo, null, 4), "utf-8");
+          copyDataJson.push({
+            active: true,
+            etat: dilaInfo.sections[0].etat,
+            id: kaliInfo.data.id,
+            nature: dilaInfo.nature,
+            num: cc.num,
+            shortTitle: kaliInfo.data.shortTitle,
+            texte_de_base: dilaInfo.texteBaseId[0],
+            title: kaliInfo.data.title,
+            url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=" + kaliInfo.data.id,
+          });
+        }
       }
     }
   }
