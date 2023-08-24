b4977133dcab27f6eeaad5db32c916bb8727f8e9
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-08-24 12:17:21 +0200

    fix: files

--- update-cc/difference.ts
+++ update-cc/difference.ts
@@ -5,76 +5,113 @@ import dataJson = require("../data/index.json");
 import { Diff, ConventionCollective } from "./types";
 
 export function getDifferenceBetweenDataAndDares(): Diff {
-  const workSheetsFromFile = xlsx.parse(`${__dirname}/data-cc/dares.xlsx`);
+    const workSheetsFromFile = xlsx.parse(`${__dirname}/data-cc/dares.xlsx`);
 
-  const supportedCcXlsx: ConventionCollective[] = [];
+    const supportedCcXlsx: ConventionCollective[] = [];
 
-  workSheetsFromFile[0].data.forEach((row: string[]) => {
-    const ccNumber = parseInt(row[0]);
-    const ccName = row[1];
-    if (ccNumber && ccName) {
-      const ccNameWithoutParenthesis = ccName.replace(/\(.*annexée.*\)/gi, "").trim();
-      supportedCcXlsx.push({
-        name: ccNameWithoutParenthesis,
-        num: ccNumber,
-      });
-    }
-  });
+    workSheetsFromFile[0].data.forEach((row: string[]) => {
+        const ccNumber = parseInt(row[0]);
+        const ccName = row[1];
+        if (ccNumber && ccName) {
+            const ccNameWithoutParenthesis = ccName.replace(/\(.*annexée.*\)/gi, "").trim();
+            supportedCcXlsx.push({
+                name: ccNameWithoutParenthesis,
+                num: ccNumber,
+            });
+        }
+    });
 
-  const supportedCcIndexJson: ConventionCollective[] = dataJson.map(cc => {
-    return {
-      name: cc.title,
-      num: cc.num,
-    };
-  });
+    const supportedCcIndexJson: ConventionCollective[] = dataJson.map(cc => {
+        return {
+            name: cc.title,
+            num: cc.num,
+        };
+    });
 
-  const ccManquante: ConventionCollective[] = supportedCcXlsx.filter(
-    ccIndex => !supportedCcIndexJson.find(ccXlsx => ccXlsx.num === ccIndex.num),
-  );
+    const ccManquante: ConventionCollective[] = supportedCcXlsx.filter(
+        ccIndex => !supportedCcIndexJson.find(ccXlsx => ccXlsx.num === ccIndex.num),
+    );
 
-  const ccEnTrop = supportedCcIndexJson.filter(
-    ccXlsx => !supportedCcXlsx.find(ccIndex => ccIndex.num === ccXlsx.num),
-  );
+    const ccEnTrop = supportedCcIndexJson.filter(
+        ccXlsx => !supportedCcXlsx.find(ccIndex => ccIndex.num === ccXlsx.num),
+    );
 
-  return { ccManquante, ccEnTrop };
+    return { ccManquante, ccEnTrop };
 }
 
 export function getDifferenceBetweenDataAndWeez(): Promise<Diff> {
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        const supportedCcXlsx: ConventionCollective[] = [];
+
+        fs.createReadStream(__dirname + "/data-cc/weez.csv")
+            .pipe(csv())
+            .on("data", data => {
+                const ccNumber = parseInt(data.IDCC);
+                if (ccNumber && !supportedCcXlsx.find(cc => cc.num === ccNumber)) {
+                    supportedCcXlsx.push({
+                        name: "?",
+                        num: ccNumber,
+                        exampleSiret: data.SIRET,
+                    });
+                }
+            })
+            .on("end", () => {
+                const supportedCcIndexJson = dataJson.map(cc => {
+                    return {
+                        name: cc.title,
+                        num: cc.num,
+                    };
+                });
+
+                const ccManquante = supportedCcXlsx.filter(
+                    ccIndex => !supportedCcIndexJson.find(ccXlsx => ccXlsx.num === ccIndex.num),
+                );
+
+                const ccEnTrop = supportedCcIndexJson.filter(
+                    ccXlsx => !supportedCcXlsx.find(ccIndex => ccIndex.num === ccXlsx.num),
+                );
+
+                resolve({ ccManquante, ccEnTrop });
+            })
+            .on("error", error => {
+                reject(error);
+            });
+    });
+}
+
+export function getDifferenceBetweenVerificationFileAndIndex(): Diff {
+    const workSheetsFromFile = xlsx.parse(`${__dirname}/data-cc/verify_cc.xlsx`);
+
     const supportedCcXlsx: ConventionCollective[] = [];
 
-    fs.createReadStream(__dirname + "/data-cc/weez.csv")
-      .pipe(csv())
-      .on("data", data => {
-        const ccNumber = parseInt(data.IDCC);
-        if (ccNumber && !supportedCcXlsx.find(cc => cc.num === ccNumber)) {
-          supportedCcXlsx.push({
-            name: "?",
-            num: ccNumber,
-            exampleSiret: data.SIRET,
-          });
+    workSheetsFromFile[0].data.forEach((row: string[]) => {
+        const ccNumber = parseInt(row[0]);
+        const ccName = row[1];
+        const ccLink = row[3];
+        if (ccNumber && ccName && ccLink) {
+            const ccNameWithoutParenthesis = ccName.replace(/\(.*annexée.*\)/gi, "").trim();
+            supportedCcXlsx.push({
+                name: ccNameWithoutParenthesis,
+                num: ccNumber,
+                link: ccLink,
+            });
         }
-      })
-      .on("end", () => {
-        const supportedCcIndexJson = dataJson.map(cc => {
-          return {
+    });
+
+    const supportedCcIndexJson: ConventionCollective[] = dataJson.map(cc => {
+        return {
             name: cc.title,
             num: cc.num,
-          };
-        });
+        };
+    });
 
-        const ccManquante = supportedCcXlsx.filter(
-          ccIndex => !supportedCcIndexJson.find(ccXlsx => ccXlsx.num === ccIndex.num),
-        );
+    const ccManquante: ConventionCollective[] = supportedCcXlsx.filter(
+        ccIndex => !supportedCcIndexJson.find(ccXlsx => ccXlsx.num === ccIndex.num),
+    );
 
-        const ccEnTrop = supportedCcIndexJson.filter(
-          ccXlsx => !supportedCcXlsx.find(ccIndex => ccIndex.num === ccXlsx.num),
-        );
+    const ccEnTrop = supportedCcIndexJson.filter(
+        ccXlsx => !supportedCcXlsx.find(ccIndex => ccIndex.num === ccXlsx.num),
+    );
 
-        resolve({ ccManquante, ccEnTrop });
-      })
-      .on("error", error => {
-        reject(error);
-      });
-  });
+    return { ccManquante, ccEnTrop };
 }
